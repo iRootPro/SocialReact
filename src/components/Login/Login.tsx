d170afb3962882210fc545ac76331275f32d4643
Alexander Neupokoev <admin@armavir.ru>
2020-09-23 10:53:25 +0300

    feat: use redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import {InjectedFormProps, reduxForm, Field} from "redux-form";
+import {log} from "util";
+
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+
+}
 
 export const Login = () => {
+    const onSubmit = (formData:FormDataType) => {
+        console.log(formData)
+    }
     return (
         <div>
-            <span>Login</span>
+            <h1>Login</h1>
+           <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>>  = (props) => {
+    return (
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={'login'}  name={'login'} component={'input'} />
+                </div>
+                <div>
+                    <Field type="text" placeholder={'password'} name={'password'} component={'input'}/>
+                </div>
+                <div>
+                    <Field type="checkbox" name={'rememberMe'} component={'input'}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+    )
+}
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,12 +4,14 @@ import {dialogsReducer} from "./dialogs-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunkMiddleWare from "redux-thunk"
+import {reducer as formReducer} from "redux-form"
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleWare))
